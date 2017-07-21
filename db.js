35ca153f90265855196579bedc831d0e37d4f5a4
Robert van der Mark <robert.vandermark@gmail.com>
2017-07-21 17:55:48 -0400

    db set up , quick express set

---
+++ db.js
@@ -0,0 +1,30 @@
+const data = {};
+
+const addCat = (cat) => 	data[cat] = [];
+const deleteCat = (cat) => delete data[cat];
+
+const addProd = (cat, prod) => {
+		id = data[cat].reduce( (memo, prod) => {
+			return prod.id >= memo ? prod.id : memo; 
+		},0) + 1;			
+	data[cat].push({name: prod, id: id });
+};
+
+const deleteProd = (cat, id) => data[cat] = data[cat].filter((prod) => prod.id !== id);
+const getCatNames = () => Object.keys(data);
+const getProdByCat = (cat) => data[cat].map((prod) => prod.name);
+
+const seed = () => {
+	addCat('Snacks');
+	addProd('Snacks', '3 Musketeers');
+	addProd('Snacks', 'Almond Pack');
+	addCat('Meats');
+	addProd('Meats', 'Fisheses');
+	addProd('Meats', 'Fat Hobbit')	
+}
+
+seed();
+
+module.exports = {addCat: addCat, deleteCat: deleteCat, addProd: addProd, deleteProd: deleteProd, getCatNames: getCatNames, getProdByCat: getProdByCat, seed: seed }
+
+console.log('data = ', data)
