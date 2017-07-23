68a8dcaf858649a8be7d1ad90951b172f329dcde
Robert van der Mark <robert.vandermark@gmail.com>
2017-07-23 12:34:47 -0400

    everything but displayed data

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
 const nunjucks =  require('nunjucks');
+const methodOverride =  require('method-override');
+const bodyParser = require('body-parser');
 let {seed, getCatNames, getProdByCat, addCat, deleteCat, addProd, deleteProd} = require('./db');
 
 const app = express();
 app.use(express.static(__dirname + '/node_modules'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine', 'html')
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true});
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
 	res.render('index', {catNames: getCatNames()} );
@@ -19,9 +23,32 @@ app.get('/categories/:category', (req, res) => {
 	res.render('category', {activeCat: cat,  products: getProdByCat(cat), catNames: getCatNames()});
 });
 
+app.delete('/categories/:category', (req, res) => {
+	deleteCat(req.params.category);
+	res.redirect('/');
+});
+
+app.delete('/categories/:category/:productId', (req, res) => {
+	const cat = req.params.category;
+	const prodId = req.params.productId
+	deleteProd(cat, prodId);
+	res.redirect(`/categories/${cat}`);
+})
+
 app.get('/seed', (req, res) => {
 	seed();
 	res.redirect('/');
+});
+
+app.post('/categories/:category', (req, res) => {
+	const cat = req.params.category;
+	addProd(cat, req.body.product);
+	res.redirect(`/categories/${cat}`);
+});
+
+app.post('/categories', (req, res) => {
+	addCat(req.body.category);
+	res.redirect(`/`);
 });
 
 const port = process.env.PORT || 3000
--- db.js
+++ db.js
@@ -1,26 +1,32 @@
-const data = {};
+let data = {};
 
-const addCat = (cat) => 	data[cat] = [];
+const addCat = (cat) => {
+		if (cat) data[cat] = [];
+};
+
 const deleteCat = (cat) => delete data[cat];
 
 const addProd = (cat, prod) => {
+	if (prod) {
 		id = data[cat].reduce( (memo, prod) => {
 			return prod.id >= memo ? prod.id : memo; 
 		},0) + 1;			
-	data[cat].push({name: prod, id: id });
+		data[cat].push({name: prod, id: id });		
+	}
 };
 
-const deleteProd = (cat, id) => data[cat] = data[cat].filter((prod) => prod.id !== id);
+const deleteProd = (cat, id) => data[cat]  = data[cat].filter((prod) => prod.id !== id*1);
 const getCatNames = () => Object.keys(data);
 const getProdByCat = (cat) => data[cat].map((prod) => prod);
 
 const seed = () => {
-	addCat('Snacks');
-	addProd('Snacks', '3 Musketeers');
-	addProd('Snacks', 'Almond Pack');
-	addCat('Meats');
-	addProd('Meats', 'Fisheses');
-	addProd('Meats', 'Fat Hobbit');
+	data = [];
+	const cats = ['Snacks', 'Meats'];
+	const prodSnacks = ['3 Musketeers', 'Almond Pack', 'Oreos', 'Doritos'];
+	const prodMeats = ['Fisheses', 'Stupid Fat Hobbit', 'Rabbits - Raw and Wrigglin\'', 'Orcs']
+	cats.forEach( cat => addCat(cat));
+	prodSnacks.forEach( prod => addProd('Snacks', prod));
+	prodMeats.forEach( prod  => addProd('Meats', prod));
 	console.log('Just Seeded the DB.');	
 }
 
